42af4ec4a52c3c8f47c9352f9f38dc8894db39d7
Manish Kumar <mkumar@numerix.com>
2017-06-21 13:31:40 +0800

    OVSG-59

--- src/test/java/com/manishk/sse/SeDemoApplicationTests.java
+++ src/test/java/com/manishk/sse/SeDemoApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SeDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
 	}
 
 }
